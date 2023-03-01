6a74d97803e80f2a20005127aa756f0382af861e
Lodkahs <omalojiu@gmail.com>
2023-03-01 20:38:33 +0200

    creating user

--- lodkagram/ViewController.swift
+++ lodkagram/ViewController.swift
@@ -7,6 +7,7 @@
 // He's my Instagram killer app
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -25,6 +26,33 @@ class ViewController: UIViewController {
     }
     
     @IBAction func signUpClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (authData, error) in
+                
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                    
+                }
+            }
+            
+        } else {
+            makeAlert(titleInput: "Error", messageInput: "Username/Password?")
+        }
+        
+        
+        
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
     
